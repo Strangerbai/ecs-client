428ab9ca2085dc27e459d39706960507dd33d4dd
baijianbiao <baijianbiao@weidian.com>
2020-04-09 19:41:51 +0800

    log

--- src/main/java/com/bee/sample/ecs/controller/UserReditRestController.java
+++ src/main/java/com/bee/sample/ecs/controller/UserReditRestController.java
@@ -1,7 +1,9 @@
 package com.bee.sample.ecs.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.bee.sample.ecs.dto.model.TbMemberInfoDO;
 import com.bee.sample.ecs.service.IMemberInfoService;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +14,15 @@ import java.util.List;
 @RestController
 public class UserReditRestController {
 
+    protected static final org.slf4j.Logger logger = LoggerFactory.getLogger(UserReditRestController.class);
+
     @Resource
     IMemberInfoService iMemberInfoService;
 
     @RequestMapping(value = "/userCredit/{status}")
     public List<TbMemberInfoDO> getCreditLevel(@PathVariable String status){
         List<TbMemberInfoDO> tbMemberInfoDOS =  iMemberInfoService.getMemberInfoByStatus(status);
+        logger.info("tbMemberInfoDOS {}", JSON.toJSONString(tbMemberInfoDOS));
         return tbMemberInfoDOS;
     }
 
--- src/main/java/com/bee/sample/ecs/service/impl/IMemberInfoServiceImpl.java
+++ src/main/java/com/bee/sample/ecs/service/impl/IMemberInfoServiceImpl.java
@@ -1,9 +1,12 @@
 package com.bee.sample.ecs.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.bee.sample.ecs.controller.UserReditRestController;
 import com.bee.sample.ecs.dto.mapper.TbMemberInfoDOMapper;
 import com.bee.sample.ecs.dto.model.TbMemberInfoDO;
 import com.bee.sample.ecs.dto.model.TbMemberInfoDOExample;
 import com.bee.sample.ecs.service.IMemberInfoService;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -11,6 +14,8 @@ import java.util.List;
 
 @Service
 public class IMemberInfoServiceImpl implements IMemberInfoService {
+
+    protected static final org.slf4j.Logger logger = LoggerFactory.getLogger(IMemberInfoServiceImpl.class);
 
     @Resource
     TbMemberInfoDOMapper tbMemberInfoDOMapper;
@@ -20,6 +25,7 @@ public class IMemberInfoServiceImpl implements IMemberInfoService {
         TbMemberInfoDOExample example = new TbMemberInfoDOExample();
         example.createCriteria().andStatusEqualTo(Byte.valueOf(status));
         List<TbMemberInfoDO> result = tbMemberInfoDOMapper.selectByExample(example);
+        logger.info("getMemberInfoByStatus result : {}", JSON.toJSONString(result));
         return result;
     }
 }
